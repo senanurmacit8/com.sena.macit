dd243cdd1661795d83514f56491e4df28ed9e9e6
sena.macit@etiya.com <sena.macit@etiya.com>
2020-04-11 17:35:58 +0300

    Initial commit

---
+++ src/main/config/AppConfig.java
@@ -0,0 +1,69 @@
+package ${package};
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+import tr.edu.duzce.mf.bm.springextjs.model.Bolum;
+import tr.edu.duzce.mf.bm.springextjs.model.Danisman;
+import tr.edu.duzce.mf.bm.springextjs.model.Fakulte;
+import tr.edu.duzce.mf.bm.springextjs.model.Ogrenci;
+
+import java.util.Properties;
+
+import static org.hibernate.cfg.Environment.*;
+
+@PropertySource("classpath:hibernate.properties")
+@EnableTransactionManagement
+@Configuration
+@ComponentScan(basePackages = {"tr.edu.duzce"})
+public class AppConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        Properties props = new Properties();
+        // Setting JDBC properties
+        props.put(DRIVER, env.getProperty("mysql.driver"));
+        props.put(URL, env.getProperty("mysql.url"));
+        props.put(USER, env.getProperty("mysql.user"));
+        props.put(PASS, env.getProperty("mysql.password"));
+
+        // Setting Hibernate properties
+        props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
+        props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+        props.put(DIALECT, env.getProperty("hibernate.dialect"));
+
+        // Setting C3P0 properties
+        props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
+        props.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
+        props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
+        props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout")); // seconds
+        props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
+        props.put(C3P0_CONFIG_PREFIX + ".initialPoolSize", env.getProperty("hibernate.c3p0.initialPoolSize"));
+        props.put(C3P0_IDLE_TEST_PERIOD, env.getProperty("hibernate.c3p0.idle_test_period")); // seconds
+        props.put(C3P0_CONFIG_PREFIX + ".acquireRetryAttempts", env.getProperty("hibernate.c3p0.acquireRetryAttempts"));
+        props.put(C3P0_CONFIG_PREFIX + ".acquireRetryDelay", env.getProperty("hibernate.c3p0.acquireRetryDelay")); // milliseconds
+
+        factoryBean.setHibernateProperties(props);
+        factoryBean.setAnnotatedClasses(Ogrenci.class, Bolum.class, Danisman.class, Fakulte.class);
+        return factoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
+    }
+
+}
